868f0e367cd2b54c46dd04c3001a6fae7f2fb5dd
Müller Kristóf <muller.kristof@itk.ppke.hu>
2024-01-17 15:10:08 +0100

    Add placeholder functions

--- pyPCG/features.py
+++ pyPCG/features.py
@@ -1,2 +1,39 @@
+import numpy as np
+import pyPCG as pcg
+import scipy.signal as signal
+
+def time_deltas(start,end):
+    pass
+
+def onset_time(start,envelope):
+    pass
+
+def exit_time(end,envelope):
+    pass
+
+def zero_cross_rate(start,end,sig):
+    pass
+
+def peak_width(start,end,envelope):
+    pass
+
+def max_freq(start,end,sig):
+    pass
+
+def spectral_spread(start,end,sig):
+    pass
+
+def cwt(start,end,sig):
+    pass
+
+def dwt(start,end,sig):
+    pass
+
+def katz_fd(start,end,sig):
+    pass
+
+def lyapunov(start,end,sig):
+    pass
+
 if __name__ == '__main__':
     print("Feature calculation")
