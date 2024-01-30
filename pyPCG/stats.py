7a57faf5af14c63ea9aee08da179429063e18a91
Müller Kristóf <muller.kristof@itk.ppke.hu>
2024-01-30 15:09:09 +0100

    Add multidim support for selected stats

--- pyPCG/stats.py
+++ pyPCG/stats.py
@@ -12,22 +12,34 @@ def outlier_remove_transform(data,dist=3.):
     return data[s<dist]
 
 def mean(data):
-    return np.mean(data)
+    if len(data.shape) == 1:
+        return np.mean(data)
+    else: return np.mean(data,axis=1)
 
 def std(data):
-    return np.std(data)
+    if len(data.shape) == 1:
+        return np.std(data)
+    else: return np.std(data,axis=1)
 
 def rms(data):
-    return np.sqrt(np.mean(data**2))
+    if len(data.shape) == 1:
+        return np.sqrt(np.mean(data**2))
+    else: return np.sqrt(np.mean(data**2,axis=1))
 
 def med(data):
-    return np.median(data)
+    if len(data.shape) == 1:
+        return np.median(data)
+    else: return np.median(data,axis=1)
 
 def skew(data):
-    return sts.skew(data)
+    if len(data.shape) == 1:
+        return sts.skew(data)
+    else: return sts.skew(data,axis=1)
 
 def kurt(data):
-    return sts.kurtosis(data)
+    if len(data.shape) == 1:
+        return sts.kurtosis(data)
+    else: return sts.kurtosis(data,axis=1)
 
 def max(data,k=1):
     s_data = np.sort(data)
@@ -38,7 +50,9 @@ def min(data,k=1):
     return s_data[k-1:k]
 
 def percentile(data,perc=25):
-    return np.percentile(data,perc)
+    if len(data.shape) == 1:
+        return np.percentile(data,perc)
+    else: return np.percentile(data,perc,axis=1)
 
 def window_operator(data,win_size,fun,overlap_percent=0.5):
     step = win_size-round(win_size*overlap_percent)
