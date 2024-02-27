e31eecb8ece280cced952b7b1a297686227c561f
Müller Kristóf <muller.kristof@itk.ppke.hu>
2024-02-27 14:40:30 +0100

    Remove dependence on partially applied functions

--- pyPCG/__init__.py
+++ pyPCG/__init__.py
@@ -1,27 +1,69 @@
 import numpy as np
+import numpy.typing as npt
 import matplotlib.pyplot as plt
 import copy
+from typing import Callable
 
 class pcg_signal:
-    def __init__(self, data=np.array([]), fs=1, log=[]) -> None:
+    """PCG signal object
+    
+    Attributes:
+    ----------
+        data (np.ndarray): signal data
+        fs (int): sampling rate
+        processing_log (list[str]): processing steps and parameters on the signal
+    
+    Methods:
+    -------
+        get_timelength: get the length of the signal in seconds
+    """
+    def __init__(self, data:npt.NDArray=np.array([]), fs:int=1, log:list[str]=["File read in"]) -> None:
         self.data = data.astype(float)
         self.fs = fs
         self.processing_log = log
+        
+    def __repr__(self) -> str:
+        return f"PCG signal [{self.fs}] {self.processing_log}"
 
     def get_timelength(self) -> float:
+        """Get legth of signal in seconds
+
+        Returns:
+            float: length of signal in seconds
+        """
         return len(self.data)/self.fs
     
 class process_pipeline:
-    def __init__(self, *args) -> None:
+    """Processing pipeline. One step's input is the prevous step's output
+    
+    Attributes:
+    ----------
+        steps (list[Callable | tuple[Callable, dict]]): List of steps as functions or function and parameters as keyword dictionary
+    
+    Methods:
+    -------
+        run(input): Run the process pipeline as described in steps
+    """
+    def __init__(self, *configs: Callable|tuple[Callable, dict[str,int|float|str]]) -> None:
         self.steps = []
-        for k in args:
-            # TODO: import inspect -> check for valid signature
+        for k in configs:
             self.steps.append(k)
     
     def run(self, input: pcg_signal) -> pcg_signal:
+        """Run the processing pipeline
+
+        Args:
+            input (pcg_signal): input signal
+
+        Returns:
+            pcg_signal: processed signal
+        """
         out = input
         for step in self.steps:
-            out = step(out)
+            if type(step) is tuple:
+                out = step[0](out,**step[1])
+            else:
+                out = step(out)
         return out
 
 def zero_center(sig: pcg_signal) -> pcg_signal:
