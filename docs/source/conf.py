f9b51761317dd2686e188b2fc5b45324b3602b76
Müller Kristóf <muller.kristof@itk.ppke.hu>
2024-04-16 14:22:44 +0200

    Remove unnecessary comments

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,27 +1,11 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'pyPCG'
 copyright = '2024, Kristóf Müller'
 author = 'Kristóf Müller'
 release = '0.1-a'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx_toolbox.more_autodoc.autotypeddict','enum_tools.autoenum']
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
