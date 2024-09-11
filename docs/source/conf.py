89aa5b5b1b9a8fb5d9c61b26b174a07441051eeb
Müller Kristóf <muller.kristof@itk.ppke.hu>
2024-09-11 13:47:35 +0200

    Add mock module for readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,3 +9,16 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+import os
+if os.environ.get('READTHEDOCS', None) == 'True':
+    import sys
+    from unittest.mock import MagicMock
+    class Mock(MagicMock):
+        @classmethod
+        def __getattr__(cls, name):
+            return MagicMock()
+
+    MOCK_MODULES = ['hsmmlearn.base']
+    for mod_name in MOCK_MODULES:
+        sys.modules[mod_name] = Mock()
