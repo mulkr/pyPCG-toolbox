8131c714d5b1fd79a4f3420ee08645c1ead8f8ee
Müller Kristóf <muller.kristof@itk.ppke.hu>
2025-05-21 11:37:31 +0200

    Single source dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+    
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    dependencies = f.read().splitlines()
 
 version = "0.1x"
 with open('pyproject.toml', 'r', encoding="utf-8") as toml:
@@ -21,6 +24,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/mulkr/pyPCG-toolbox/",
     project_urls={"Bug Tracker": "https://github.com/mulkr/pyPCG-toolbox/issues",},
+    license="GPL-3.0-only",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3.10",
@@ -29,16 +33,6 @@ setup(
         "Topic :: Scientific/Engineering"
     ],
     packages=["pyPCG"],
-    install_requires=[  "scikit-learn>=1.1.3",
-                        "scipy>=1.9.3",
-                        "PyWavelets>=1.5.0",
-                        "pandas>=2.0.0",
-                        "matplotlib>=3.6.2",
-                        "numpy>=1.25.2",
-                        "hsmmlearn @ git+https://github.com/jvkersch/hsmmlearn@master",
-                        "emd>=0.6.2",
-                        "nolds>=0.5.2",
-                        "tqdm>=4.64.1",
-                        "pyQt5>=5.15.7"],
+    install_requires=dependencies,
     python_requires=">=3.10"
 )
